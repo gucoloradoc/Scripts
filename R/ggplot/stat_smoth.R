41237c392c6c095fe88c68feb7a6bb874f0a3fd3
Gerson U Colorado C <zurjak88@gmail.com>
2016-07-29 09:16:40 -0500

    Create stat_smoth.R

---
+++ R/ggplot/stat_smoth.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(datasets)
+
+# Plot 1: Jittered scatter plot, add a linear model (lm) smooth:
+ggplot(Vocab, aes(x = education, y = vocabulary)) +
+  geom_jitter(alpha = 0.2) + stat_smooth(method="lm", se=FALSE)
+
+# Plot 2: Only lm, colored by year
+ggplot(Vocab, aes(x = education, y = vocabulary, col= factor(year))) +
+  stat_smooth(method= "lm", se=FALSE)
+
+# Plot 3: Set a color brewer palette
+ggplot(Vocab, aes(x = education, y = vocabulary, col= factor(year))) +
+  stat_smooth(method= "lm", se=FALSE) +
+  scale_color_brewer()
+
+# Plot 4: Add the group, specify alpha and size
+ggplot(Vocab, aes(x = education, y = vocabulary, col = year, group= year)) +
+  stat_smooth(method = "lm", se = F, alpha=0.6, size=2) +
+  scale_color_gradientn(colors = brewer.pal(9,"YlOrRd"))
